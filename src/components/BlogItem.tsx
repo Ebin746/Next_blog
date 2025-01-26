247c403b776a09128954427f5a7fd07abc668551
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2025-01-26 23:58:32 +0530

    fix:type check in blogItem

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -5,37 +5,43 @@ import Link from "next/link";
 const BlogItem = (
   {
     title,
-  description,
-  image,
-  id,
-  category,
+    description,
+    image,
+    id,
+    category,
 
-}
+  }:{
+    title: string;
+  description: string;
+  image: string;
+  id: string;
+  category: string;
+  }
 ) => {
   return (
     <div className="max-w-[270px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_1px_#000000]">
       <Link href={`/blogs/${id}`}>
-      <Image
-        src={image}
-        width={400}
-        height={400}
-        className="border border-solid"
-        alt={title}
-      />
-      <p className="ml-3 mt-5 bg-black text-sm w-fit px-1 tracking-tighter text-gray-50">
-        {category}
-      </p>
-      <div className="p-5">
-        <h3 className="mb-3 tracking-tight text-lg font-medium text-gray-900  ">
-          {title}
-        </h3>
-        <p className="mb-3 tracking-tight text-sm ">
-          {description}
+        <Image
+          src={image}
+          width={400}
+          height={400}
+          className="border border-solid"
+          alt={title}
+        />
+        <p className="ml-3 mt-5 bg-black text-sm w-fit px-1 tracking-tighter text-gray-50">
+          {category}
         </p>
-        <div className="inline-flex items-center text-center py-2 font-semibold">
-          read more <Image src={assets.arrow} alt="arrow" />
+        <div className="p-5">
+          <h3 className="mb-3 tracking-tight text-lg font-medium text-gray-900  ">
+            {title}
+          </h3>
+          <p className="mb-3 tracking-tight text-sm ">
+            {description}
+          </p>
+          <div className="inline-flex items-center text-center py-2 font-semibold">
+            read more <Image src={assets.arrow} alt="arrow" />
+          </div>
         </div>
-      </div>
       </Link>
     </div>
   );
