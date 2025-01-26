749d9a167d86881b42860a5cfd3e3660d2d7ee06
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2025-01-26 23:56:16 +0530

    feat: Add layout and sidebar components for admin interface

---
+++ src/components/AdminComponents/SideBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '../../../public/assets/assets'
+const SideBar = () => {
+  return (
+    <div className='flex flex-col bg-slate-50'>
+        <div className="px-3 py-3 sm:pl-14 border border-black">
+        <Image src={assets.logo} alt="logo" width={120} />
+        </div>
+        <div className="w-28 sm:w-80 h-[100vh] relative border border-black">
+            <div className="flex items-center border border-black gap-3 font-medium px-3 py-3 bg-white shadow-[-5px_5px_0px_#000000]">
+                <Image src={assets.add_icon} alt="logo" width={28} className='rounded-3xl'/> <p>Add blog</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
